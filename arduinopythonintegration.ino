b9e3bb8d49026a01b8b5852fd5c6cf11f9fe1ad5
bharath96sripathy <36510926+bharath96sripathy@users.noreply.github.com>
2018-03-26 23:22:28 +0530

    Add files via upload

---
+++ arduinopythonintegration.ino
@@ -0,0 +1,24 @@
+char serialData;
+
+
+void setup() {
+  pinMode(13, OUTPUT);
+  Serial.begin(9600);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  if(Serial.available() > 0){
+    serialData = Serial.read();
+    Serial.print(serialData);
+
+    if(serialData == '1'){
+      digitalWrite(13, HIGH);}
+    else if(serialData != '1'){
+        digitalWrite(13, LOW);}
+    }
+}
+  // put your main code here, to run repeatedly:
+
+
